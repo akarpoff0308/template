bfa439f5df1ace3ecb69b46345f76976efaa2a70
akarpoff0308 <akarpoff308@yandex.ru>
2025-07-04 18:48:19 +0200

    prettier added

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +15,12 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'prettier'
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+],
+eslintConfigPrettier)
